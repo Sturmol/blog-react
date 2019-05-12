806547839213ec5e3e686b7c613be392207567c2
Miguel Guelbenzu <miguel.guelbenzu@aperto.com>
2019-05-12 16:40:14 +0200

    Update reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,4 @@
 import {combineReducers} from "redux";
+import postsReducer from './postsReducer'
 
-export default combineReducers({dummyReducer: () => 'dummy reducer to be replace'});
+export default combineReducers({posts: postsReducer});
