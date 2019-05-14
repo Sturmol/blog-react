0dcffd5fc9feb9e97bc46165372a60e8bee86a45
Miguel Guelbenzu <miguel.guelbenzu@aperto.com>
2019-05-14 12:08:49 +0200

    Add Reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,23 @@
-
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => async dispatch => {
-	const response = await jsonPlaceholder.get('/posts');
+//ActionCreator inside Actioncreator
 
-	dispatch({type: 'FETCH_POSTS', payload: response.data })
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+	await dispatch(fetchPosts())
+	const userIds = new Set(getState().posts.map(({ userId }) => userId))
+	userIds.forEach(id => dispatch(fetchUser(id)))
+};
+
+// Normal Syntax
+export const fetchPosts =  () => {
+	return async (dispatch, getState) =>{
+		const response =  await jsonPlaceholder.get('/posts');
+		dispatch({type : 'FETCH_POST', payload : response.data})
+	};
+};
+
+//Shortened Syntax
+export const fetchUser  = (id) => async dispatch => {
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+	dispatch({type : 'FETCH_USER', payload : response.data});
 };
